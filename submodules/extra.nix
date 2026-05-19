687ce8db1702b75dc4d0f5c5812f0dbea150d8dd
Aleksandr-86 <Aleksandr-Karpenko-86@yandex.ru>
2026-05-19 09:16:04 +0300

    выполнена генерация и настройка самоподписных сертификатов

--- submodules/extra.nix
+++
@@ -1,8 +0,0 @@
-{
-  environment.shellAliases = {
-    zapret = "cd ~/repos/zapret-linux-flake && sudo nix run";
-    yandex-install = "cd ~/nixos-dotfiles && nix profile add github:miuirussia/yandex-browser.nix#yandex-browser-stable && nix flake update yandex-browser";
-    cfg = "cd ~/nixos-dotfiles && nvim .";
-    dev = "cd ~/projects/tenderhelp_python/th-front";
-  };
-}
